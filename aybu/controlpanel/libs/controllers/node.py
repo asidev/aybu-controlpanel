4b310898ac46a2cf4346e74fa8316541a3ac5981
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-09-02 17:48:20 +0200

    Fixed bugs in 'load_entity_from_string'. Added 'load_validator_from_string' in utils.py Finished function 'aybu.controlpanel.libs.controllers.node.create'.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110902154820-n0v0mact2xtvqi5j

--- aybu/controlpanel/libs/controllers/node.py
+++ aybu/controlpanel/libs/controllers/node.py
@@ -1,11 +1,17 @@
 
+from aybu.controlpanel.libs.utils import load_entity_from_string
+import logging
+
+
+log = logging.getLogger(__name__)
+
 
 def index(session, type_, **kwargs):
     """
         Retrieve and return data to display menus trees.
         HINT: load __FIRST__ level of each menu.
     """
-    # Check node type
+    # Check node type. 
     # Validate params (based on node type).
     # Call model function to get wanted data.
     return None
@@ -14,16 +20,33 @@ def create(session, type_, **kwargs):
     """
         Create and return a new node.
     """
-    # Check node type
-    # Validate params (based on node type).
-    # Call model function to get wanted data.
-    return None
+    try:
+        Entity = load_entity_from_string(type_)
+
+    except ValueError as e:
+        msg = 'type_: entity %s does not exist!' % type_
+        log.debug(msg)
+        raise TypeError(msg)
+
+    try:
+        validator = load_validator_from_string('validate_%s' % Entity.__name__)
+
+    except ValueError as e:
+        log.debug('A validator for %s does not exist!' % Entity.__name__)
+
+    else:
+        validator(**kwargs)
+
+    entity = Entity(**params)
+    session.add(entity)
+
+    return entity
 
 def update(session, type_, **kwargs):
     """
         Update and return a node.
     """
-    # Check node type
+    # Check node type.
     # Validate params (based on node type).
     # Call model function to get wanted data.
     return None
@@ -32,7 +55,7 @@ def delete(session, type_, **kwargs):
     """
         Delete a node.
     """
-    # Check node type
+    # Check node type.
     # Validate params (based on node type).
     # Call model function to get wanted data.
     return None
@@ -41,7 +64,7 @@ def search(session, type_, **kwargs):
     """
         Perform a search in node collection and return results.
     """
-    # Check node type
+    # Check node type.
     # Validate params (based on node type).
     # Call model function to get wanted data.
     return None
--- aybu/controlpanel/libs/utils.py
+++ aybu/controlpanel/libs/utils.py
@@ -1,16 +1,25 @@
 
 import aybu.controlpanel.models as models
+import aybu.controlpanel.validators as validators
 
 def load_entity_from_string(entity):
-    """ Load an entity class from the models using its string name.
+    """ Load an entity class from the models using its string name."""
 
-        Return the argument unchanged when it is not a string.
-    """
+    if not isinstance(entity, basestring):
+        entity = str(entity)
 
-    if not isinstance(type_, basestring):
-        return entity
+    if hasattr(models, entity):
+        return getattr(models, entity)
 
-    if hasattr(models, type_):
-        return getattr(models, type_)
+    raise ValueError('No entity named %s exists in the model.' % entity)
 
-    raise ValueError('No entity named %s in the model.' % entity)
+def load_validator_from_string(validator):
+    """ Load a validator callable from 'validators' package using its name."""
+
+    if not isinstance(validator, basestring):
+        validator = str(validator)
+
+    if hasattr(models, validator):
+        return getattr(models, validator)
+
+    raise ValueError('No validator named %s exists.' % validator)
