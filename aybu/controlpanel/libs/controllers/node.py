75dbf44d57612a78246cdc7d5d9208d3aaa6a578
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-09-02 15:43:50 +0200

    Added functions in the Node controller module.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110902134350-p2sb9t7r8minxet0

---
+++ aybu/controlpanel/libs/controllers/node.py
@@ -0,0 +1,47 @@
+
+
+def index(session, type_, **kwargs):
+    """
+        Retrieve and return data to display menus trees.
+        HINT: load __FIRST__ level of each menu.
+    """
+    # Check node type
+    # Validate params (based on node type).
+    # Call model function to get wanted data.
+    return None
+
+def create(session, type_, **kwargs):
+    """
+        Create and return a new node.
+    """
+    # Check node type
+    # Validate params (based on node type).
+    # Call model function to get wanted data.
+    return None
+
+def update(session, type_, **kwargs):
+    """
+        Update and return a node.
+    """
+    # Check node type
+    # Validate params (based on node type).
+    # Call model function to get wanted data.
+    return None
+
+def delete(session, type_, **kwargs):
+    """
+        Delete a node.
+    """
+    # Check node type
+    # Validate params (based on node type).
+    # Call model function to get wanted data.
+    return None
+
+def search(session, type_, **kwargs):
+    """
+        Perform a search in node collection and return results.
+    """
+    # Check node type
+    # Validate params (based on node type).
+    # Call model function to get wanted data.
+    return None
