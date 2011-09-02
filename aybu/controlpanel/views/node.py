f10a1a4b897d7c43b3fe549b2d5a7f7f15ba7233
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-09-02 15:31:43 +0200

    Renamed old "views.py" to "node.py". Added node views: they will be the HTTP API for Node.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110902133143-5pbjpb9s0w1zgwf9

---
+++ aybu/controlpanel/views/node.py
@@ -0,0 +1,52 @@
+
+
+def index(context, request):
+    """
+        This views is called when the user enters in
+        the control panel section named "Pages management".
+        The view must load data to display menus trees.
+        HINT: load __FIRST__ level of each menu.
+    """
+    # Parsing the request
+    # Call controller index to list node tree: don't load all tree!!!
+    # Prepare response
+    return None
+
+def create(context, request):
+    """
+        This views is called to create a new node.
+        Node type must be specified.
+    """
+    # Parsing the request
+    # Call controller create.
+    # Prepare response
+    return None
+
+def update(context, request):
+    """
+        This views is called to update a node.
+        Node id and type must be specified.
+    """
+    # Parsing the request
+    # Call controller update.
+    # Prepare response
+    return None
+
+def delete(context, request):
+    """
+        This views is called to delete node.
+        Node id and type must be specified.
+    """
+    # Parsing the request
+    # Call controller delete.
+    # Prepare response
+    return None
+
+def search(context, request):
+    """
+        This views is called to perform a search in node collection.
+    """
+    # Parsing the request
+    # Call controller update.
+    # Prepare response
+    return None
