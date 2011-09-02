4c99ad93ff75f249363fbd460e8765b54caf96cb
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-09-02 15:59:58 +0200

    Created a structure in test cases class to reflect functions in "libs.controllers" and "views" packages. Each test body raises an NotImplementedError.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110902135958-nwu03sxslf0lld32

--- tests/test_libs/test_controllers/test_node.py
+++ tests/test_libs/test_controllers/test_node.py
@@ -1,25 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
+from pyramid import testing
+import logging
 try:
     import unittest2 as unittest
 except:
     import unittest
 
-from pyramid import testing
-from logging import getLogger
 
-from babel import Locale
-
-from aybu.website.lib.database import fill_db
-from aybu.website.models import Menu, Page, Section, InternalLink, ExternalLink
-from aybu.website.models import Node, View, NodeInfo
-from aybu.controlpanel.lib.structure import check_url_part, boolify
-from aybu.controlpanel.lib.structure import is_valid_parent, create_node
-
-import random
-
-log = getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 class NodeControllersTests(unittest.TestCase):
@@ -27,39 +15,20 @@ class NodeControllersTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
 
-        from aybu.website.models.base import Base
-        from sqlalchemy import create_engine
-        from sqlalchemy.orm import scoped_session
-        from sqlalchemy.orm import sessionmaker
-
-        self.engine = create_engine('sqlite:///:memory:')
-        self.session = scoped_session(sessionmaker())
-        self.session.configure(bind=self.engine)
-
-        Base.metadata.drop_all(self.engine)
-        Base.metadata.create_all(self.engine)
-
-        fill_db(self.session)
-
     def tearDown(self):
-        self.session.remove()
         testing.tearDown()
 
-    def test_create_node(self):
+    def test_index(self):
+        raise NotImplementedError()
 
-        nodes = self.session.query(Node).all()
+    def test_create(self):
+        raise NotImplementedError()
 
-        for i in xrange(0, len(nodes)):
-            parent = nodes[i]
+    def test_update(self):
+        raise NotImplementedError()
 
-            if is_valid_parent(parent):
-                self.assertIsInstance(create_node(self.session, type_=Section,
-                                                  parent=parent), Section)
-            else:
-                self.assertRaises(ValueError, create_node, self.session,
-                                  type_=Section, parent=parent)
+    def test_delete(self):
+        raise NotImplementedError()
 
-            self.assertRaises(ValueError,create_node, self.session,
-                              type_=NodeInfo, parent=parent)
-
-            self.session.rollback()
+    def test_search(self):
+        raise NotImplementedError()
