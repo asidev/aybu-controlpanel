fba03473bbb731e81920a49e13ea1fba50db9468
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-09-03 18:53:19 +0200

    Added validators needed by 'Node' controller: 'validate_lineage' and 'validate_node'.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110903165319-wk4387c9t55dhp6u

---
+++ aybu/controlpanel/libs/validators/__init__.py
@@ -0,0 +1,5 @@
+
+from node import validate_lineage
+from node import validate_node
+
+__all__ = []
---
+++ aybu/controlpanel/libs/validators/node.py
@@ -0,0 +1,20 @@
+
+def validate_lineage(cls, ancestor):
+    """Check if 'cls' is a subclass of 'ancestor'.
+
+    """
+
+    if not issubclass(cls, ancestor):
+        raise ValueError('%s is not a subclass of %s.' % (cls.__name__,
+                                                          ancestor.__name__))
+
+def validate_node(id=None, enabled=None, hidden=None, weight=None, 
+                  parent=None, children=None):
+
+    """ Validate input values that can be used to create a Node instance.
+
+        Function signature contains all attributes of Node
+        but 'id', 'enabled', 'hidden', 'weight' and 'children' are not checked.
+    """
+    if not isinstance(parent, (Page, Menu, Section)):
+        raise ValueError('%s cannot have children.' % parent.__name__))
