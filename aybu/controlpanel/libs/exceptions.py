e1c0a8036680b3a1cc60e96e5f742e7222503a7a
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-09-05 10:32:43 +0200

    Added the test for 'libs.exceptions.ValidationError'. I used it to fix bugs.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110905083243-jv9f4rs5ahnltd0l

--- aybu/controlpanel/libs/exceptions.py
+++ aybu/controlpanel/libs/exceptions.py
@@ -1,13 +1,29 @@
 
-class ValidationError(Exception):
+import logging
 
-    def __init__(self, message, *args):
-        super(ValidationError, self).__init__(message, *args)
+log = logging.getLogger(__name__)
+
+
+class ValidationError(Exception):
+    """This class must be used by validators when they fail during checks.
+
+        'ValidationError' is equal to 'Exception', 
+        but its string representation is different because
+        it force string formatting using self.args.
+        String formatting can fail, in this case 'ValidationError.__repr__' 
+        return the same result of 'Exception.__repr__'.
+    """
+    def __init__(self, *args):
+        super(ValidationError, self).__init__(*args)
 
     def __repr__(self):
 
-        if len(self.args) > 1:
-            return self.args[0] % self.args
+        try:
+            return self.args[0] % self.args[1:]
 
-        return self.args[0]
+        except (TypeError, IndexError) as e:
+            log.debug('String formatting fails: %s', e)
+            return super(ValidationError, self).__repr__()
 
+    def __str__(self):
+        return self.__repr__()
