334a70c32cad6ffe9148a29af5ba8a647ce168cc
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-10-31 18:14:02 +0100

    Ported StructureHandler.tree and StructureHandler.link_list. Added tests.

--- aybu/controlpanel/handlers/structure.py
+++ aybu/controlpanel/handlers/structure.py
@@ -16,23 +16,65 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from aybu.core.models import (Language, 
+                              Menu,
+                              Node,
+                              ExternalLink,
+                              InternalLink,
+                              Page)
 from pyramid_handlers import action
 from . base import BaseHandler
+import logging
 
 
 __all__ = ['StructureHandler']
+
+log = logging.getLogger(__name__)
 
 
 class StructureHandler(BaseHandler):
 
     @action(renderer='json')
     def tree(self):
-        raise NotImplementedError
+        # FIXME: add beaker session.
+        # language = Language.get(self.session, client_session['lang'].id)
+        language = Language.get(self.session, 1)
+        # FIXME: don't load all database!!!
+        # HINT: add parameter 'level' to the request to load first N levels.
+        return [node.to_dict(language)
+                for node in Node.all(self.session) 
+                if isinstance(node, Menu) and node.parent == None]
 
-    @action()
+    @action(renderer='string')
     def link_list(self):
-        # TODO: omg this returns raw javascript!
-        raise NotImplementedError
+        """ Return a javascript with tinyMCELinkList array.
+        """
+        self.request.response.content_type = "application/javascript"
+        try:
+            # FIXME: add beaker session.
+            # language = Language.get(self.session, client_session['lang'].id)
+            language = Language.get(self.session, 1)
+
+            items = []
+            for page in Page.all(self.session):
+                translation = page.get_translation(language)
+                item = '["{}","{}"]'.format(translation.title.replace('"', "'"),
+                                            translation.url)
+                items.append(item)
+
+            response = 'var tinyMCELinkList = new Array(%s);' % ', '.join(items)
+
+        except Exception as e:
+            log.exception(e)
+            self.session.rollback()
+            response = '/* ' + str(e) + ' */'
+            self.request.response.status = 500
+
+        else:
+            self.session.commit()
+            self.request.response.status = 200
+
+        return response
 
     @action(renderer='json')
     def list(self):
