57663450dbf1ce5bd87026e195fc6ffaa3cbbcf6
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-12-22 15:46:25 +0100

    Fixed copyright and contributors

--- aybu/controlpanel/handlers/background.py
+++ aybu/controlpanel/handlers/background.py
@@ -1,5 +1,5 @@
 """
-Copyright 2010 Asidev s.r.l.
+Copyright 2010-2012 Asidev s.r.l.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
